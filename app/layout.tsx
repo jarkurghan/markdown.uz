201ee91a651e2dda4c2e1580f91dbcf2b142c614
nnazirovdev <j.arkurghan@gmail.com>
2026-08-16 12:56:29 +0500

    Set dynamic runtime behavior in RootLayout to force dynamic rendering

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { ThemeProvider } from "@/contexts/theme-context";
 import { Tashrif } from "tashrif/react";
 import "./globals.css";
 
+export const dynamic = "force-dynamic";
+
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
     await connection();
     const tashrifClientId = process.env.NEXT_PUBLIC_TASHRIF_CLIENT_ID;
